1269557dd7e050102d70c6f1a95b96c622aefa35
alamin0561464977 <amin23435678901315@gmail.com>
2022-11-08 09:45:07 +0300

    Added a Services api

--- index.js
+++ index.js
@@ -8,12 +8,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.db_user_password)
-
 app.get('/', (req, res) => {
     res.send('Photographer Project Running')
 });
 
+
+// ==============================================================================
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_user_name}:${process.env.db_user_password}@cluster0.blopt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const servicesCollection = client.db("photographer").collection("services");
+
+        app.get('/services', async (req, res) => {
+            const services = await servicesCollection.find({}).toArray();
+            res.send(services);
+        })
+
+    }
+    finally {
+
+    }
+};
+run().catch(err => console.log(err))
+
+// ==============================================================================
+
 app.listen(port, () => {
     console.log('Photographer Project Running on Port', port)
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
